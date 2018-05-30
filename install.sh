40f7a7adf8e452275c9ce9be0904a52549675588
668-010 <911@668-010.ru>
2018-05-30 17:01:55 +0500

    fix package name  for proxmox 4.4

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt update
-apt install -y libssl1.0-dev libx11-dev libpcsclite-dev pcscd libgss-dev build-essential make libkrb5-dev libgssglue-dev
+apt install -y libssl-dev libx11-dev libpcsclite-dev pcscd libgss-dev build-essential make libkrb5-dev libgssglue-dev
 cd ./rdesktop-1.8.3
 ./configure
 make clean
